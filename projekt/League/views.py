0af717b16b4477ee5e5f110907dd74c0b1b01469
Michał Górkiewicz <michal.gorkiewicz@student.wat.edu.pl>
2021-06-25 08:56:06 +0200

    1

--- projekt/League/views.py
+++ projekt/League/views.py
@@ -3,14 +3,14 @@ from django.template import loader
 from django.http import HttpResponse
 from .models import Pozycja, Postac, Rola
 
-import cassiopeia as cass
+
 
 
 
 
 def index(request):
     wszystkie = Postac.objects.all()
-    test = cass.get_champions()
+
     dane = {'kategorie': wszystkie}
 
-    return render(request, 'szablon1.html', test)
+    return render(request, 'szablon1.html', dane)
